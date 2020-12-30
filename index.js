ce38347ff14694a27ae1bf2b432884bc3debf5f1
ak <madnankhalid.bukc@gmail.com>
2020-12-30 15:53:37 +0500

    host to heruku

--- index.js
+++ index.js
@@ -1,48 +1,48 @@
-const app = require("express")();
-const bodyParser = require("body-parser");
-const cheerio = require("cheerio");
-const mongoose = require("mongoose");
-const cors = require("cors");
-const session = require("express-session");
-const schedule = require("node-schedule");
+const app = require('express')()
+const bodyParser = require('body-parser')
+const cheerio = require('cheerio')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const session = require('express-session')
+const schedule = require('node-schedule')
 
-const port = process.env.PORT || 5000;
-const uri = process.env.MONGO_URI;
+const port = process.env.PORT || 5000
+const uri = process.env.MONGO_URI
 
 // Connecting to Database
-(async () => {
-    mongoose.set("useCreateIndex", true);
-    mongoose.set("useFindAndModify", false);
-    await mongoose
-        .connect(uri, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        })
-        .then(() => {
-            console.log("MongoDB Connected…");
-        })
-        .catch((err) => console.log(err));
-})();
+;(async () => {
+  mongoose.set('useCreateIndex', true)
+  mongoose.set('useFindAndModify', false)
+  await mongoose
+    .connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      console.log('MongoDB Connected…')
+    })
+    .catch((err) => console.log(err))
+})()
 
 // Use Middlewares
-app.use(cors());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 app.use(
-    session({
-        secret: "Bukc AK",
-        resave: true,
-        saveUninitialized: true,
+  session({
+    secret: 'Bukc AK',
+    resave: true,
+    saveUninitialized: true,
 
-        cookie: {
-            maxAge: 1000 * 60 * 60, // (1000 -> msec * 60 -> sec * 60 -> min * 24 -> hrs * 1 -> days)
-        },
-    }),
-);
+    cookie: {
+      maxAge: 1000 * 60 * 60, // (1000 -> msec * 60 -> sec * 60 -> min * 24 -> hrs * 1 -> days)
+    },
+  }),
+)
 
-app.use("/lms/users", require("./routes/user"));
-app.use("/lms/crawler", require("./routes/crawl"));
+app.use('/lms/users', require('./routes/user'))
+app.use('/lms/crawler', require('./routes/crawl'))
 
 // schedule.scheduleJob('*/2 * * * *', () => separation());
-
-app.listen(port, () => console.log("App is running!"));
+app.set('port', process.env.NODE_PORT)
+// app.listen(port, () => console.log("App is running!"));
